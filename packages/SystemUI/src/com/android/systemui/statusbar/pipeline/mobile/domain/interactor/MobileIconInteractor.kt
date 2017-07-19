3926792ebff8d006f4e301acfd24021fdc7f5ccd
Pranav Vashi <neobuddy89@gmail.com>
2017-07-19 23:46:45 +0530

    SystemUI: Allow using 4G icon instead of LTE [1/2]
    [someone5678] Adapt to SystemUI mobile connections pipeline
    shuttercat: adapt to A16 and A16QPR1 kairos changes
    
    Co-authored-by: Dmitrii <bankersenator@gmail.com>
    Change-Id: Ife3f11237d12c6332da0ce4ba81c97b51eb9075f
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/domain/interactor/MobileIconInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/domain/interactor/MobileIconInteractor.kt
@@ -44,7 +44,9 @@ import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.conflate
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
@@ -153,6 +155,8 @@ interface MobileIconInteractor {
 
     /** See [MobileIconsInteractor.isVoWifiForceHidden]. */
     val isVoWifiForceHidden: Flow<Boolean>
+
+    val shouldShowFourgIcon: StateFlow<Boolean>
 }
 
 /** Interactor for a single mobile connection. This connection _should_ have one subscription ID */
@@ -190,7 +194,7 @@ class MobileIconInteractorImpl(
             "system:" + Settings.System.DATA_DISABLED_ICON;
 
     private val shouldShowExclamationMark: StateFlow<Boolean> =
-        conflatedCallbackFlow {
+        callbackFlow {
                 val callback =
                     object : TunerService.Tunable {
                         override fun onTuningChanged(key: String, newValue: String?) {
@@ -204,6 +208,32 @@ class MobileIconInteractorImpl(
 
                 awaitClose { Dependency.get(TunerService::class.java).removeTunable(callback) }
             }
+            .conflate()
+            .stateIn(
+                scope,
+                started = SharingStarted.WhileSubscribed(),
+                true
+            )
+
+    private final val SHOW_FOURG_ICON: String =
+            "system:" + Settings.System.SHOW_FOURG_ICON;
+
+    override val shouldShowFourgIcon: StateFlow<Boolean> =
+        callbackFlow {
+                val callback =
+                    object : TunerService.Tunable {
+                        override fun onTuningChanged(key: String, newValue: String?) {
+                            when (key) {
+                                SHOW_FOURG_ICON -> 
+                                    trySend(TunerService.parseIntegerSwitch(newValue, false))
+                            }
+                        }
+                    }
+                Dependency.get(TunerService::class.java).addTunable(callback, SHOW_FOURG_ICON)
+
+                awaitClose { Dependency.get(TunerService::class.java).removeTunable(callback) }
+            }
+            .conflate()
             .stateIn(
                 scope,
                 started = SharingStarted.WhileSubscribed(),
