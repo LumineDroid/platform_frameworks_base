9753ca7235f768658e7e61266017b10a7304a107
Jis G Jacob <studiokeys@blissroms.org>
2024-10-02 02:53:24 -0400

    fw/b: Fix Tuner crashes
    Fixes:
    
    java.lang.RuntimeException: Unable to start activity ComponentInfo{com.android.systemui/com.android.systemui.tuner.TunerActivity}: java.lang.IllegalStateException: This Activity already has an action bar supplied by the window decor. Do not request Window.FEATURE_ACTION_BAR and set android:windowActionBar to false in your theme to use a Toolbar instead.
    
    Change-Id: I22539d0d6385511de1c5a31a277b76b1c05805f0
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
@@ -18,6 +18,7 @@ package com.android.systemui.tuner;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -59,7 +60,9 @@ public class TunerActivity extends CollapsingToolbarBaseActivity implements
     }
 
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.tuner_activity);
 
         DynamicColors.applyToActivityIfAvailable(this);
