b58c226f1dcf57a00dab389d50480529a86c5c94
Ido Ben-Hur <idoybh2@gmail.com>
2026-01-11 23:55:33 +0200

    SystemUI: Keep custom shade blur default as the old overlay [1/2]
    max_shade_window_blur_radius

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationShadeDepthController.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationShadeDepthController.kt
@@ -69,6 +69,7 @@ import dagger.Lazy
 import java.io.PrintWriter
 import javax.inject.Inject
 import kotlin.math.max
+import kotlin.math.roundToInt
 import kotlin.math.sign
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -267,17 +268,19 @@ constructor(
         }
 
     private fun updateMaxBlurRadius() {
+        val metrics = context.resources.displayMetrics
+        val dps = maxBlurRadiusPx / metrics.density
         val radiusDpSetting = Settings.System.getIntForUser(
             context.contentResolver,
             Settings.System.SHADE_BLUR_RADIUS,
-            48,
+            dps.roundToInt(),
             UserHandle.USER_CURRENT
         )
 
         userDefinedMaxBlurRadius = TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP,
             radiusDpSetting.toFloat(),
-            context.resources.displayMetrics
+            metrics
         ).toInt()
         scheduleUpdate()
     }
@@ -524,7 +527,8 @@ constructor(
             }
         }
         context.contentResolver.registerContentObserver(
-            Settings.System.getUriFor("shade_blur_radius"), false, settingsObserver, UserHandle.USER_ALL
+            Settings.System.getUriFor(Settings.System.SHADE_BLUR_RADIUS),
+            false, settingsObserver, UserHandle.USER_ALL
         )
         updateMaxBlurRadius()
 
