efd31d3a860348310b4d59f4df6c3e3ff255f328
Ghosuto <clash.raja10@gmail.com>
2026-04-20 11:47:29 +0000

    SystemUI: Improve notification panel boost
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -2130,7 +2130,6 @@ public final class NotificationPanelViewController implements
     }
 
     private void onTrackingStarted() {
-        boostInteraction(700);
         endClosing();
         mShadeRepository.setLegacyShadeTracking(true);
         if (mTrackingStartedListener != null) {
@@ -2843,8 +2842,8 @@ public final class NotificationPanelViewController implements
             mExpanding = true;
             mIsExpandingOrCollapsing = true;
             mQsController.onExpandingStarted(mQsController.getFullyExpanded());
+            boostInteraction(700);
         }
-        boostInteraction(700);
     }
 
     void notifyExpandingFinished() {
